5285c21bba8355ae9d9d318574888dfcf60e9972
Alina <alinadakhno60@gmail.com>
2021-04-09 14:15:59 +0200

    contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -4,12 +4,12 @@ const Contact = () => {
     return (
         <div className="contact">
             <h1>Contact me!</h1>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5919.68878610711!2d4.914794529327474!3d52.39281906879494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c60850e72d7677%3A0x45d58620cc999fd1!2sAzaleastraat%202%2C%201032%20BW%20Amsterdam!5e0!3m2!1sen!2snl!4v1617002595174!5m2!1sen!2snl"  frameborder="0"
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d155959.8924297995!2d4.763878108967292!3d52.354582834316005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c63fb5949a7755%3A0x6600fd4cb7c0af8d!2sAmsterdam!5e0!3m2!1sen!2snl!4v1617970450127!5m2!1sen!2snl"  frameborder="0"
             allowFullScreen=""
             aria-hidden="false"
             tabIndex="0">
             </iframe>
-            <h4>Phone</h4>
+             <h4>Phone</h4>
             <p>+31630792944</p>
             <h4>Email</h4>
             <p>alinadakhno60@gmail.com</p>
